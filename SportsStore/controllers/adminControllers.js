8134f28235c3ebd03b70c003fec3cf2d8790e82e
unknown <you@example.com>
2018-09-16 11:22:28 +0600

    first commit

---
+++ SportsStore/controllers/adminControllers.js
@@ -0,0 +1,58 @@
+﻿/// <reference path="../angular.min.js" />
+
+angular.module("sportsStoreAdmin")
+.constant("authUrl", "https://parseapi.back4app.com/login/")
+  .constant("ordersUrl", "https://parseapi.back4app.com/classes/Orders/")
+.run(function ($http) {
+    $http.defaults.headers.common["X-Parse-Application-Id"]
+    = "nAbnnPr7IkQiLSfd7AAHOC3nNLM3wrv3OYWJw3kb";
+    $http.defaults.headers.common["X-Parse-REST-API-Key"]
+            = "zpR26VeA7sZfofI7MDHaV53RhPwSzKJ46IKcLN3B";
+})
+.controller("authCtrl", function ($scope, $http, $location, authUrl) {
+    //$scope.authenticate = function (user, pass) {
+    //    $http.get(authUrl, {
+    //        params: {
+    //            username: user,
+    //            password: pass
+    //        },
+    //    }).then(function (data) {
+    //        $scope.$broadcast("sessionToken", data.sessionToken);
+    //        $http.defaults.headers.common["X-Parse-Session-Token"]
+    //        = data.sessionToken;
+    //        $location.path("/main");
+    //    }), function (error) {
+    //        $scope.authenticationError = error;
+    //    }
+    //}
+}).controller("mainCtrl", function ($scope) {
+    //$scope.screens = ["Products", "Orders"];
+    //$scope.current = $scope.screens[0];
+
+    //$scope.setScreen = function (index) {
+    //    $scope.current = $scope.screens[index];
+    //};
+    //$scope.getScreen = function () {
+    //    return $scope.current == "Products"
+    //    ? "/views/adminProducts.html" : "/views/adminOrders.html";
+    //};
+}).controller("ordersCtrl", function ($scope, $http, ordersUrl) {
+    $http.get(ordersUrl)
+    .then(function (data) {
+        $scope.orders = data.results;
+    }), function (error) {
+        $scope.error = error;
+    }
+    $scope.selectedOrder;
+
+    $scope.selectOrder = function (order) {
+        $scope.selectedOrder = order;
+    };
+    $scope.calcTotal = function (order) {
+        var total = 0;
+        for (var i = 0; i < order.products.length; i++) {
+            total += order.products[i].count * order.products[i].price;
+        }
+        return total;
+    }
+});
---
+++ SportsStore/controllers/sportsStore.js
@@ -0,0 +1,50 @@
+﻿/// <reference path="../angular.min.js" />
+
+angular.module("SportsStore")
+
+    .constant("dataUrl", "https://parseapi.back4app.com/classes/Products/")
+    .constant("orderUrl", "https://parseapi.back4app.com/classes/Orders/")
+    .run(function ($http) {
+        $http.defaults.headers.common["X-Parse-Application-Id"]
+            = "nAbnnPr7IkQiLSfd7AAHOC3nNLM3wrv3OYWJw3kb";
+        $http.defaults.headers.common["X-Parse-REST-API-Key"]
+            = "zpR26VeA7sZfofI7MDHaV53RhPwSzKJ46IKcLN3B";
+    })
+
+    //.controller("sportsStoreCtrl", function ($scope) {
+    //    $scope.data = {
+    //        products: [
+    //            { name: "Product #1", description: "A product", category: "Category #1", price: 100 },
+    //            { name: "Product #2", description: "A product", category: "Category #1", price: 110 },
+    //            { name: "Product #3", description: "A product", category: "Category #2", price: 210 },
+    //            { name: "Product #4", description: "A product", category: "Category #3", price: 202 }
+    //        ]
+    //    };
+    //});
+
+    .controller("sportsStoreCtrl", function ($scope, $http,$location, dataUrl,orderUrl,cart) {
+        $scope.data = {};
+        $http.get(dataUrl)
+            .then(function (data) {
+                $scope.data.products = data;
+            }, function (response) {
+                $scope.data.error = response.error || response;
+            })
+        $scope.sendOrder = function (shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            $http.post(orderUrl, order)
+            .then(function (data) {
+                $scope.data.orderId = data;
+                cart.getProducts().length == 0;
+            }, function (error) {
+                $scope.data.orderError = error;
+            }).finally(function () {
+                $location.path("/complete")
+            })
+        }
+            //.error(function (response) {
+            //    $scope.data.error = response.error || response;
+            //    console.log('error');
+        //})
+    });
